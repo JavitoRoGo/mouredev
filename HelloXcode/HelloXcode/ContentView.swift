4e73bba62bdb3e31bb229339da055978ec10cfd6
JavitoRoGo <javierrodriguezgomez79@gmail.com>
2023-04-16 14:11:18 +0100

    Se añade HelloXcode

---
+++ HelloXcode/HelloXcode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloXcode
+//
+//  Created by Javier Rodríguez Gómez on 27/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("¡Hola mundo!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            
+            
+            
+    }
+}
